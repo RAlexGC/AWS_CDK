986d4fa376b3e9261431be6db5219034edd8bc1a
Ramón Gutiérrez <rgutierrez@inair.studio>
2023-01-19 15:33:13 -0600

    UPDATE: Updates on handling errors in lambda function

--- lambda/mylambda.ts
+++ lambda/mylambda.ts
@@ -7,24 +7,51 @@ export const handler: Handler = async (event: any): Promise<any> => {
   const bucketName = process.env.ASSETBUCKET || "";
   const apiKey = process.env.AIRTABLE_KEY;
   const baseID = process.env.AIRTABLE_BASE;
-
-  const base = new Airtable({apiKey: apiKey}).base(baseID);
-
-  // Deserialize the body from the event
-  const info = JSON.parse(event.body);
-  console.log(event);
-
+  let base;
+  try {
+    base = new Airtable({ apiKey: apiKey }).base(baseID);
+  } catch (e: any) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "Airtable error",
+        error: e.name,
+      }),
+    };
+  }
+  let info;
+  try {
+    // Deserialize the body from the event
+    info = JSON.parse(event.body);
+  } catch (e: any) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "JSON body error",
+        error: e.name,
+      }),
+    };
+  }
   // Define how the image will be saved on S3
   const imageKey = `images/${info.name}.${info.extension}`;
-
-  // Fetch the image from the provided URL using Axios
-  const { data } = await axios.get(info.url, {
-    responseType: "arraybuffer",
-  });
-
+  let response;
+  try {
+    // Fetch the image from the provided URL using Axios
+    response = await axios.get(info.url, {
+      responseType: "arraybuffer",
+    });
+  } catch (e: any) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "Axios error",
+        error: e.name,
+      }),
+    };
+  }
+  const data = response.data;
   // Create a buffer from the image data
   const buf = Buffer.from(data);
-
   // Create the command to send the image to S3
   const command = new PutObjectCommand({
     Bucket: bucketName,
@@ -32,27 +59,47 @@ export const handler: Handler = async (event: any): Promise<any> => {
     Body: buf,
     ACL: "public-read",
   });
-
-  // Save the image with the s3Client
-  const client = new S3Client({});
-  const clientData = await client.send(command);
-  const baseObjectURL = "https://imagestorage-awstutorialrgfafa2a8f-nv5mrudxh5mv.s3.amazonaws.com/"
-
-  // Update URL field on Airtable
-  await base(info.tableID).update([
-    {
-      "id": info.recordID,
-      "fields": {
-        "fldy5CHeXKcDLRSYB": baseObjectURL + imageKey,
-        "fldES3LlRKuqDlxJi": info.name,
-        "fldjq4QZsCPfdW4qQ": info.extension
-      }
-    }
-  ]);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Image uploaded to S3!",
-    }),
-  };
+  let baseObjectURL;
+  try {
+    // Save the image with the s3Client
+    const client = new S3Client({});
+    const clientData = await client.send(command);
+    baseObjectURL =
+      "https://imagestorage-awstutorialrgfafa2a8f-nv5mrudxh5mv.s3.amazonaws.com/";
+  } catch (e: any) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "S3 Client error",
+        error: e.name,
+      }),
+    };
+  }
+  try {
+    // Update URL field on Airtable
+    await base(info.tableID).update([
+      {
+        id: info.recordID,
+        fields: {
+          fldy5CHeXKcDLRSYB: baseObjectURL + imageKey,
+          fldES3LlRKuqDlxJi: info.name,
+          fldjq4QZsCPfdW4qQ: info.extension,
+        },
+      },
+    ]);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Image uploaded to S3!",
+      }),
+    };
+  } catch (e: any) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "Airtable error",
+        error: e.name,
+      }),
+    };
+  }
 };
